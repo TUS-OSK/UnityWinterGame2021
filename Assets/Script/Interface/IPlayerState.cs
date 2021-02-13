5dcdd44889c02f2eea06c46d06751aa0322fecb0
hitoiki <hitoiki103@gmail.com>
2021-02-13 13:39:28 +0900

    addInterface

---
+++ Assets/Script/Interface/IPlayerState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+interface IPlayerState
+{
+    ReactiveProperty<int> GetHP();
+    ReactiveProperty<int> GetMoves();
+}
