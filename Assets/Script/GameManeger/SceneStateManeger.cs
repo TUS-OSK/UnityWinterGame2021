1d061a312cbbe366d38f581e72b726b0f335d020
hitoiki <hitoiki103@gmail.com>
2021-02-20 16:46:18 +0900

    SceneStateManeger雛形

---
+++ Assets/Script/GameManeger/SceneStateManeger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class SceneStateManeger : MonoBehaviour
+{
+    IPlayerState player;
+    IPuzzleState puzzle;
+    IKeyPad keyPad;
+    [SerializeField] SceneState sceneState;
+
+    GameMode statebaf;
+
+    private void Start()
+    {
+        Init();
+    }
+    private void Init()
+    {
+        player.GetHP().Where(x => x <= 0).Subscribe(
+            _ => { if (sceneState.gameMode != GameMode.clear) sceneState.gameMode = GameMode.gameover; }
+        );
+
+        keyPad.PhaseChangeKey().Where(x => x == true).Subscribe(
+            _ =>
+            {
+                if (sceneState.gameMode == GameMode.shooting) sceneState.gameMode = GameMode.puzzle;
+                else if (sceneState.gameMode == GameMode.puzzle) sceneState.gameMode = GameMode.shooting;
+            }
+        );
+
+        keyPad.MenuKey().Where(x => x == true).Subscribe(
+            _ =>
+            {
+                if (sceneState.gameMode != GameMode.gameover && sceneState.gameMode != GameMode.clear)
+                {
+                    statebaf = sceneState.gameMode;
+                    sceneState.gameMode = GameMode.menu;
+                }
+            }
+        );
+
+        puzzle.ClearFlag().Where(x => x == true).Subscribe(
+            _ =>
+            {
+                sceneState.gameMode = GameMode.clear;
+            }
+        );
+    }
+}
---
+++ Assets/Script/GameManeger/UiInjecter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class UiInjecter : MonoBehaviour
+{
+    IPlayerState player;
+    IPuzzleState puzzle;
+    [SerializeField] SceneState sceneState;
+
+    [SerializeField] Text hp;
+    [SerializeField] GameObject MenuCambas;
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        player.GetHP().Subscribe(
+            x => { hp.text = x.ToString(); }
+        );
+    }
+}
