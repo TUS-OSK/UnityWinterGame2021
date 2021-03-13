2790a3e913e6d32e5090b42153e9d0a78fefa3f8
1419012@ed.tus.ac.jp <>
2021-03-13 14:43:19 +0900

    現状バグありです

--- Assets/Script/Pazzle/Pazzle.cs
+++ Assets/Script/Pazzle/Pazzle.cs
@@ -8,32 +8,118 @@ public class Pazzle : MonoBehaviour
      // タイルの種類
     public enum TileType
     {
-        NONE, // 何も無い
+        NONE,   // 何も無い
         GROUBD, // 地面
-        CANDY, // キャンディー
-        BLOCK, // ブロック
-        SPONE, // スポーンブロック
+        CANDY,  // キャンディー
+        BLOCK,  // ブロック
+        SPONE,  // スポーンブロック
 
         SCORE, // 点数
     }
 
+    private int rows = 7;             // 行数    // 変更予定
+    private int columns  = 7;         // 列数    // 変更予定
+    public TextAsset stageFile; // ステージ構造が記述されたテキストファイル
+
+    // タイルの情報を読み込む
+    private void LoadTileData()
+    {
+        // タイルの情報を一行ごとに分割
+        var lines = stageFile.text.Split
+        (
+            new[] { '\r', '\n' },
+            System.StringSplitOptions.RemoveEmptyEntries
+        );
+
+        // タイルの列数を計算
+        var nums = lines[ 0 ].Split( new[] { ',' } );
+
+        // タイルの列数と行数を保持
+        rows = lines.Length; // 行数
+        columns = nums.Length; // 列数
+
+        // タイル情報を int 型の２次元配列で保持
+        tileList = new TileType[ columns, rows ];
+        for ( int y = 0; y < rows; y++ )
+        {
+            var st = lines[ y ];
+            nums = st.Split( new[] { ',' } );
+            for ( int x = 0; x < columns; x++ )
+            {
+                tileList[ x, y ] = ( TileType )int.Parse( nums[ x ] );
+            }
+        }
+    }
+
     public Sprite groundSprite;  // 地面のスプライト
     public Sprite blockSprite;   // ブロックのスプライト
-
-    private int rows = 7;        // 行数
-    private int columns = 7;     // 列数
     public TileType[,] tileList; // タイル情報を管理する二次元配列
+    public int[,] color;         // キャンディの色を管理する二次元配列
+
+    private Vector3 middleOffset;// 中心位置
     public float tileSize;       // タイルのサイズ
 
     // ステージを作成
     private void CreateStage()
     {
 
-        int[,] tileList = new int [rows,columns];
+        tileList = new TileType[ columns, rows ];
+        color = new int[ columns, rows ];
+        int K_ = 5;  // K種類
+
+        for ( int y = 0; y < rows; y++ )
+        {
+            for ( int x = 0; x < columns; x++ )
+            {
+                tileList[ x, y ] = ( TileType ) 1;
+                color[ x, y ] = K_;
+            }
+        }
 
         // ステージの中心位置を計算
         middleOffset.x = columns * tileSize * 0.5f - tileSize * 0.5f;
         middleOffset.y = rows * tileSize * 0.5f - tileSize * 0.5f;
+
+        // キャンディの配置を乱数生成 
+
+        for ( int y = 0; y < rows; y++ )
+        {
+            for ( int x = 0; x < columns; x++ )
+            {
+                if ( tileList[ x, y ] == TileType.NONE ) 
+                {
+                    color[ x, y ] = K_;
+                    continue;
+                }
+                List<int> v_ = new List<int>();  // 配置可能な種類を入れる配列
+                for(int p = 0; p < K_; p++)
+                {
+                    bool flag = true;
+                    if(x >= 2)
+                    {
+                        if( p == color[x - 1, y] && p == color[x - 2, y])
+                        {
+                            flag = false;
+                        }
+                    }
+                    if(y >= 2){
+                        if( p == color[x, y - 1] && p == color[x, y - 2])
+                        {
+                            flag = false;
+                        }
+                    }
+                    if(flag)
+                    {
+                        v_.Add(p);
+                    }
+                }
+                color[x,y] = v_[Random.Range(0, v_.Count)];
+            }
+        }
+
+        // タイルの貼り付け
+
+        int z = 0;  // 変更予定
 
         for ( int y = 0; y < rows; y++ )
         {
@@ -51,14 +137,26 @@ public class Pazzle : MonoBehaviour
                 // タイルのスプライトを設定
                 sr.sprite = groundSprite;
                 // タイルの位置を設定
-                tile.transform.position = GetDisplayPosition( x, y );
+                tile.transform.position = GetDisplayPosition( x , y , z );   
             }
         }
+    }
+
+    // 指定された行番号と列番号からスプライトの表示位置を計算して返す
+    private Vector3 GetDisplayPosition( int x, int y , int z )
+    {
+        return new Vector3
+        (
+            x *  tileSize - middleOffset.x,
+            y * -tileSize + middleOffset.y,
+            z
+        );
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        // LoadTileData(); // タイルの情報を読み込む
         CreateStage(); // ステージを作成
     }
 
